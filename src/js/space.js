eaa36cb41363d6bff55b863e549f37dc13ebec4e
uozmann <1974155655@qq.com>
2023-08-05 20:50:31 -0400

    changed photos for cocoon

--- src/js/space.js
+++ src/js/space.js
@@ -156,7 +156,7 @@ let digitalProjects = [
 		description: "Cocoon is a collapsible mobile installation where one is invited to be immersed in an audiovisual environment and interact with the artifact. This artifact is designed to trigger, stimulate and evoke diverse sentiments that would encourage the user to explore and reflect upon their personal emotions and be aware of how sounds can affect them in a safe space. By pressing your hand on different surfaces with different forces, sound and light motifs will change depending on the area and pressure force.",
 		btn: "./assets/visuals/space/cocoonProcessBook.pdf",
 		video: "https://www.youtube.com/embed/Wm54eH6TRTs",
-		images: ["./assets/visuals/space/cocoon0.jpg", "./assets/visuals/space/cocoon1.jpg", "./assets/visuals/space/cocoon2.jpg", "./assets/visuals/space/cocoon3.jpg"]
+		images: ["./assets/visuals/space/cocoon.jpg", "./assets/visuals/space/cocoon4.jpg", "./assets/visuals/space/cocoon5.jpg", "./assets/visuals/space/cocoon6.jpg"]
 	},
 	{
 		title: "CP3 Container",
